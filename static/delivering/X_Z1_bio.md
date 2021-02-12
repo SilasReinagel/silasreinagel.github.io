afaddb7e5fd1f1cd5c1a1ff019e18ad2d5f365d7
Silas Reinagel <silas.reinagel@carvana.com>
2021-02-12 13:07:05 -0700

    Generate Letter-Size PDF

---
+++ static/delivering/X_Z1_bio.md
@@ -0,0 +1,9 @@
+# About The Author
+
+Silas Reinagel is a software engineer, a project manager, a software blogger, and a musician. He lives in sunny Phoenix, Arizona with his wife, his dog, and very close to 9 out of his 10 younger siblings. 
+
+He has worked in offices for over 20 years.
+
+As a software engineer, Silas has been creating software solutions for over 15 years, and has worked in Manufacturing, Medical, Legal, eCommerce, Hospitality, Internet Privacy, and Cryptocurrency industries.
+
+As a project manager, Silas has organized international teams to deliver a variety of software projects: video games, microservices, libraries, and desktop applications. 
---
+++ static/delivering/generate-pdf-letter.bat
@@ -0,0 +1,26 @@
+@echo off
+set dest=%1
+call mdpdf 01_version.md %dest%/001.pdf --format=Letter
+call mdpdf 02_dedication.md %dest%/002.pdf --format=Letter
+call mdpdf 03_index.md %dest%/003.pdf --format=Letter
+call mdpdf 04_intro.md %dest%/004.pdf --format=Letter
+call mdpdf C1_no_bullshit.md %dest%/010.pdf --format=Letter
+call mdpdf C2_iron_triangle.md %dest%/020.pdf --format=Letter
+call mdpdf C3_project_momentum.md %dest%/030.pdf --format=Letter
+call mdpdf C4_project_predictability.md %dest%/040.pdf --format=Letter
+call mdpdf C5_essential_tools.md %dest%/050.pdf --format=Letter
+call mdpdf C6_agile_good_and_bad.md %dest%/060.pdf --format=Letter
+call mdpdf C7_kanban_good_and_bad.md %dest%/070.pdf --format=Letter
+call mdpdf C8_scrum_good_and_bad.md %dest%/080.pdf --format=Letter
+call mdpdf C9_assembling_your_team.md %dest%/090.pdf --format=Letter
+call mdpdf C10_motivating_your_team.md %dest%/100.pdf --format=Letter
+call mdpdf C11_communicating_results.md %dest%/110.pdf --format=Letter
+call mdpdf C12_all_the_secrets.md %dest%/120.pdf --format=Letter
+call mdpdf X_A1_story_core_competencies.md %dest%/130.pdf --format=Letter
+call mdpdf X_Z1_bio.md %dest%/140.pdf --format=Letter
+
+cd %dest%
+mkdir book
+call pdftk *.pdf cat output book/delivering-book.pdf
+call start book/delivering-book.pdf
+cd %~dp0
